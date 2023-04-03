c3fa4723acc149f491589481c07b826b78145dfe
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-04-03 14:39:15 -0300

    going live

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,18 +41,18 @@ function Home() {
       is-flex-direction-column
       is-align-items-center
       is-size-2">
-      <h1 >{translate ? 'Front-end Developer' : 'Desenvolvedor Front-end'}</h1>
+      <h1 >{translate ? 'Full Stack Developer.' : 'Desenvolvedor Full Stack.'}</h1>
       <h2>{translate ? 'Hello!' : 'Ola!'}</h2>
       <p>{translate ? 'My name is' : 'Meu nome  é'}</p>
       <p className="fade">{name}</p>
       </div>
       <div className="mt-5">
       <h2 className="about-header is-size-4">{ translate ? 'About me...' : 'Sobre mim...'}</h2>
-      <p className="is-size-5">{ translate ? <>I live in Caraguatatuba, São Paulo, Brasil, and I am 46 years old. <br></br>I have been working with HTML, CSS, JavaScript, React, Redux, Context API, Bulma, and currently learning Back-end.<br></br>I am constantly seeking to improve my skills and stay up-to-date with the latest in web development.<br></br> I beleive in resilience and hard work are key ingredients to create success in anything you want to do.<br></br>Thank you for considering me as a potential member of your team.'</> 
+      <p className="is-size-5">{ translate ? <>I live in Caraguatatuba, São Paulo, Brasil, and I am 46 years old. <br></br>I have been working with HTML, CSS, JavaScript, React, Redux, Context API, Bulma, and currently learning Docker, MySQL, Sequelizer and REST API.<br></br>I am constantly seeking to improve my skills and stay up-to-date with the latest in web development.<br></br> I beleive in resilience and hard work are key ingredients to create success in anything you want.<br></br>Thank you for considering me as a potential member of your team.'</> 
 
       : <> Eu moro em Caraguatatuba, São Paulo, Brasil, e tenho 46 anos de idade. <br></br>
-      Tenho trabalhado com HTML , CSS, JavaScript, React, Redux, Context API, Bulma, e atualmente aprendendo Back-end.<br></br> Estou constantemente procurando melhorar minhas habilidades e me manter atualizado com as novidades de web development.<br></br>
-      Acredito que resiliencia e trabalhado duro sao os ingredientes para criar qualquer coisa que queira. <br></br>
+      Tenho trabalhado com HTML , CSS, JavaScript, React, Redux, Context API, Bulma, e atualmente aprendendo Docker, MySQL, Sequelizer e REST API.<br></br> Estou constantemente procurando melhorar minhas habilidades e me manter atualizado com as novidades de web development.<br></br>
+      Acredito que resiliencia e trabalhado duro sao os ingredientes para criar qualquer coisa. <br></br>
       Obrigado por me considerar como um potencial membro do seu time.</>}
       </p>
     </div>
